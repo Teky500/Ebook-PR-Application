297931dbf9b629da9a5efc5dc7c86d31030a2c84
Mohab <mohab.elsayed@bluwave-ai.com>
2024-01-20 23:11:30 -0400

    parsing of spreadsheet done

--- source/makePython/parse_excel.py
+++ source/makePython/parse_excel.py
@@ -1,2 +1,4 @@
-from urllib.request import urlretrieve
-url = 
+import pandas as pd
+xfile = pd.read_excel('source/spreadsheets/spreadsheet_1.xlsx', sheet_name= "PA-Rights")
+print(xfile)
+xfile.to_csv('source/spreadsheets/csv.csv')
