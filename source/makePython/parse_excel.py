57c07db8ccf2ac90f88521b520162ca19e192f12
Mohab <mohab.elsayed@bluwave-ai.com>
2024-01-22 19:12:51 -0400

    changes

--- source/makePython/parse_excel.py
+++ source/makePython/parse_excel.py
@@ -1,5 +1,5 @@
 import pandas as pd
-def parse_excel():
-    xfile = pd.read_excel('source/storage/spreadsheets/spreadsheet_1.xlsx', sheet_name= "PA-Rights")
-    print(xfile)
-    xfile.to_csv('source/storage/spreadsheets/spreadsheet_1.csv')
+
+xfile = pd.read_excel('source/storage/spreadsheets/spreadsheet_1.xlsx', sheet_name= "PA-Rights")
+print(xfile)
+xfile.to_csv('source/storage/spreadsheets/spreadsheet_1.csv')
