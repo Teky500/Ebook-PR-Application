0975b097c62628d6a71eac3ae01fa7df3214772d
Teky500 <mohabemad@gmail.com>
2024-02-26 22:05:47 -0400

    Search Results Changes

--- source/program/driver/features/helpers/search.py
+++ source/program/driver/features/helpers/search.py
@@ -7,22 +7,16 @@ def search_title_substring(title, db_path):
   cursor = db.cursor()
   cursor.execute(f"SELECT * FROM books where title LIKE '%{title}%'")
   result = cursor.fetchall()
-  print(result)
+  return result
 def search_ISBN(ISBN, db_path):
   db = start_connection(db_path)
   cursor = db.cursor()
   cursor.execute(f"SELECT * FROM books where ISBN = '{ISBN}'")
   result = cursor.fetchall()
-  if result == []:
-    print("No Results Found!")
-  else:
-    print(result)
+  return result
 def search_OCN(OCN, db_path):
   db = start_connection(db_path)
   cursor = db.cursor()
   cursor.execute(f"SELECT * FROM books where OCN = '{OCN}'")
   result = cursor.fetchall()
-  if result == []:
-    print("No Results Found!")
-  else:
-    print(result)
+  return result
