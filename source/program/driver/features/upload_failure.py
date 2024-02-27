0bd0af889fa4b9c0138e00408e52cddf854e3ca7
Ayesha Munir <ayeshamunir@Ayeshas-MacBook-Pro.local>
2024-02-26 20:16:51 -0400

    creates success and error pages

---
+++ source/program/driver/features/upload_failure.py
@@ -0,0 +1,28 @@
+import sys
+from PyQt6.uic import loadUi
+from PyQt6.QtWidgets import QWidget, QApplication, QStackedWidget, QPushButton, QFileDialog, QLabel
+from Themes import Theme, getTheme
+from helpers.manual_upload import man_upload
+
+class UploadSpreadsheet(QWidget):
+    def __init__(self):
+        super(UploadSpreadsheet, self).__init__()
+        self.filePicked = ''
+
+        loadUi("source/program/driver/features/ui/uploadpage_failure.ui", self)
+
+        theme = Theme(getTheme())
+        themeColour = theme.getColor()
+        if themeColour == "default":
+            pass
+        else:
+            self.setStyleSheet(f'background-color: {themeColour};')
+
+
+    def close_window(self):
+        self.window().close()
+
+app = QApplication(sys.argv)
+window = UploadSpreadsheet()
+window.show()
+sys.exit(app.exec())
