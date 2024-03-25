5b44b8772425c1639ac826e0683c9ee20f27a009
Teky500 <mohabemad@gmail.com>
2024-03-25 17:19:49 -0300

    fix for search '

--- source/features/helpers/search.py
+++ source/features/helpers/search.py
@@ -8,24 +8,29 @@ def search_title_substring(title, db_path):
   for lt in title:
     if lt == '*':
       newtitle += '%'
+    elif lt == "'":
+      newtitle += '\''
     else:
       newtitle += lt
   db = start_connection(db_path)
   cursor = db.cursor()
-  cursor.execute(f"SELECT books.*, platforms.platform  FROM BOOKS JOIN PLATFORMS ON Books.spreadsheet  = platforms.spreadsheet where title LIKE '{newtitle}'")
+  query = f"SELECT books.*, platforms.platform  FROM BOOKS JOIN PLATFORMS ON Books.spreadsheet  = platforms.spreadsheet where title LIKE ?"
+  cursor.execute(query, (newtitle, ))
   result = cursor.fetchall()
   return result
 def search_ISBN(ISBN, db_path):
   ISBN = ISBN.strip()
   db = start_connection(db_path)
   cursor = db.cursor()
-  cursor.execute(f"SELECT books.*, platforms.platform  FROM BOOKS JOIN PLATFORMS ON Books.spreadsheet  = platforms.spreadsheet where ISBN = '{ISBN}'")
+  query = f"SELECT books.*, platforms.platform  FROM BOOKS JOIN PLATFORMS ON Books.spreadsheet  = platforms.spreadsheet where ISBN = ?"
+  cursor.execute(query, (ISBN, ))
   result = cursor.fetchall()
   return result
 def search_OCN(OCN, db_path):
   OCN = OCN.strip()
   db = start_connection(db_path)
   cursor = db.cursor()
-  cursor.execute(f"SELECT books.*, platforms.platform FROM BOOKS JOIN PLATFORMS ON Books.spreadsheet  = platforms.spreadsheet where OCN = '{OCN}'")
+  query = f"SELECT books.*, platforms.platform  FROM BOOKS JOIN PLATFORMS ON Books.spreadsheet  = platforms.spreadsheet where ISBN = ?"
+  cursor.execute(query, (OCN, ))
   result = cursor.fetchall()
   return result
