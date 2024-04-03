9830133cd8fe04bc1cc70e90269f407329fdc9f1
Teky500 <mohabemad@gmail.com>
2024-04-02 21:34:10 -0300

    small fix

--- source/features/SearchDriverPage.py
+++ source/features/SearchDriverPage.py
@@ -75,7 +75,7 @@ class searchPageDriver(QtWidgets.QWidget):
             msg = QtWidgets.QMessageBox()
             msg.setWindowTitle("messageBox")
             if getLanguage() == 1:
-                msg.setText("Aucune correspondance trouvée!")
+                msg.setText("Le champ de recherche ne peut pas être vide!")
             else:
                 msg.setText("Search field can not be empty!")
             msg.setIcon(QtWidgets.QMessageBox.Icon.Information)
@@ -110,7 +110,7 @@ class searchPageDriver(QtWidgets.QWidget):
                     msg = QtWidgets.QMessageBox()
                     msg.setWindowTitle("messageBox")
                     if getLanguage() == 1:
-                        msg.setText("Aucun correspondance trouvée")
+                        msg.setText("Aucune correspondance trouvée!")
                     else:
                         msg.setText("No matches found!")
                     msg.setIcon(QtWidgets.QMessageBox.Icon.Information)
